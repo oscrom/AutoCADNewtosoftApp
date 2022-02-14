caba6fbf7683afe058dd7efa00a3df40dc17c90b
Oscar Romero <oromero@esri.com>
2022-02-14 10:56:17 -0800

    add sample application

---
+++ RuntimeApplication/SampleApplication.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Autodesk.AutoCAD.Runtime;
+using RuntimeApplication;
+
+[assembly: ExtensionApplication(typeof(SampleApplication))]
+namespace RuntimeApplication
+{
+  public class SampleApplication : IExtensionApplication
+  {
+    public void Initialize()
+    {
+      try
+      {
+        var json = "{\"menu\":{\"id\":\"file\",\"value\":\"File\",\"popup\":{\"menuitem\":[{\"value\":\"New\",\"onclick\":\"CreateNewDoc()\"},{\"value\":\"Open\",\"onclick\":\"OpenDoc()\"},{\"value\":\"Close\",\"onclick\":\"CloseDoc()\"}]}}}";
+        JsonConvert.DeserializeObject(json);
+      }
+      catch (Exception ex)
+      {
+
+      }
+    }
+
+    public void Terminate()
+    {
+
+    }
+  }
+}
---
+++ RuntimeApplication/SampleCommandClass.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Runtime;
+using RuntimeApplication;
+using Newtonsoft.Json;
+
+[assembly: CommandClass(typeof(SampleCommandClass))]
+namespace RuntimeApplication
+{
+    public class SampleCommandClass
+    {
+        [CommandMethod("SAMPLE", "SAMPLE_TestWPFCommand", CommandFlags.NoPaperSpace)]
+        public static void TestWPFMethod()
+        {
+        }
+    }
+}
